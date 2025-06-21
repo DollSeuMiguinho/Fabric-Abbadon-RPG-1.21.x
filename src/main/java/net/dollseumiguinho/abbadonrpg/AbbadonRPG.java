83fbb4c789bdf7c68c001072da326534ace8c5f0
Salgado <lucas.lemos.bertao@gmail.com>
2025-06-21 11:05:02 -0300

    Initial commit

---
+++ src/main/java/net/dollseumiguinho/abbadonrpg/AbbadonRPG.java
@@ -0,0 +1,16 @@
+package net.dollseumiguinho.abbadonrpg;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AbbadonRPG implements ModInitializer {
+	public static final String MOD_ID = "abbadonrpg";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/dollseumiguinho/abbadonrpg/AbbadonRPGClient.java
@@ -0,0 +1,10 @@
+package net.dollseumiguinho.abbadonrpg;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AbbadonRPGClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
